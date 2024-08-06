a9d4e836932531898d8f0f22f719b6b0dbbb58fd
naeljeff <naelj15@gmail.com>
2024-08-06 09:01:15 +0700

    masih ngebug yg details

--- fastapi/api/deps.py
+++ fastapi/api/deps.py
@@ -1,5 +1,3 @@
-# For dependency
-
 from typing import Annotated
 from sqlalchemy.orm import Session
 from fastapi import Depends, HTTPException, status
@@ -11,18 +9,16 @@ import os
 
 from .database import SessionLocal
 
-# Load data from env
 load_dotenv()
 SECRET_KEY = os.getenv('AUTH_SECRET_KEY')
 ALGORITHM = os.getenv('AUTH_ALGORITHM')
 
-# Dependency for database
 def getDbConnection():
-     db = SessionLocal()
-     try:
-         yield db
-     finally:
-         db.close()
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
          
 db_dependency = Annotated[Session, Depends(getDbConnection)]
 
@@ -32,11 +28,9 @@ oauth_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 oauth_bearer_dependency = Annotated[str, Depends(oauth_bearer)]
 
-# GET USER
 async def getCurrentUser(token: oauth_bearer_dependency):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        print(f"Decoded Payload: {payload}")  # Debugging
         username: str = payload.get('sub')
         userId: int = payload.get('id')
         
@@ -46,7 +40,4 @@ async def getCurrentUser(token: oauth_bearer_dependency):
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
 
-
-user_dependency = Annotated[dict, Depends (getCurrentUser)]
-         
-        
+user_dependency = Annotated[dict, Depends(getCurrentUser)]
--- fastapi/api/routers/details.py
+++ fastapi/api/routers/details.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
-from typing import Optional, Dict
 from fastapi import APIRouter, HTTPException, Depends, status
+from sqlalchemy.orm import Session
 
 from api.models import Todo, Details
 from api.deps import db_dependency, user_dependency
@@ -16,12 +16,11 @@ class DetailBase(BaseModel):
 class DetailCreate(DetailBase):
     pass
 
-# Base endpoint to get detail
 @router.get('/')
 def get_detail(
     todo_id: int,
-    db = Depends(db_dependency),
-    user = Depends(user_dependency)
+    db: Session = Depends(db_dependency),
+    user: dict = Depends(user_dependency)
 ):
     detail_instance = db.query(Details).filter(Details.todo_id == todo_id).first()
 
@@ -29,9 +28,13 @@ def get_detail(
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Detail not found')
     return detail_instance
 
-# Create new detail
 @router.post('/', status_code=status.HTTP_201_CREATED)
-def create_detail(todo_id: int, detail: DetailCreate, db = Depends(db_dependency), user = Depends(user_dependency)):
+def create_detail(
+    todo_id: int, 
+    detail: DetailCreate, 
+    db: Session = Depends(db_dependency), 
+    user: dict = Depends(user_dependency)
+):
     todo = db.query(Todo).filter(Todo.id == todo_id).first()
 
     if not todo:
@@ -45,9 +48,12 @@ def create_detail(todo_id: int, detail: DetailCreate, db = Depends(db_dependency
     db.refresh(db_detail)
     return db_detail
 
-# Delete detail
 @router.delete('/')
-def delete_detail(todo_id: int, db = Depends(db_dependency), user = Depends(user_dependency)):
+def delete_detail(
+    todo_id: int, 
+    db: Session = Depends(db_dependency), 
+    user: dict = Depends(user_dependency)
+):
     detail = db.query(Details).filter(Details.todo_id == todo_id).first()
 
     if detail:
